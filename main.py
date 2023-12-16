6b25b121584591682f0a05637c07e4d5d3950107
KLU4V <nikita.ivanus@gmail.com>
2023-12-16 14:08:10 +0300

    Blocks physics

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pygame
+import random
 
 pygame.init()
-pygame.display.set_caption('Движущийся круг 2')
-size = width, height = 600, 600
+pygame.display.set_caption('Jump to space')
+size = width, height = 576, 800
 running = True
 
 blocks = pygame.sprite.Group()
@@ -30,29 +31,45 @@ class Hero(pygame.sprite.Sprite):
 
 
 class Blocks(pygame.sprite.Sprite):
+    image_placedTitle = load_image("placed_tile.png")
+    image_placedTitle = pygame.transform.scale(image_placedTitle, (48, 48))
+    image = load_image("tile.png")
+    image = pygame.transform.scale(image, (48, 48))
 
     def __init__(self):
         super().__init__(all_sprites)
-
         self.add(blocks)
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        flag = True
+        while flag:
+            self.rect.x = random.randint(1, 10) * 48
+            for i in blocks:
+                if pygame.sprite.collide_rect(self, i):
+                    flag = False
+                else:
+                    flag = True
+        self.rect.y = 0
 
     def update(self):
-        pass
+        if 0 <= self.rect.y + 48 < 800:
+            self.rect = self.rect.move(0, 2)
+
+        elif self.rect.y + 48 == 800:
+            self.image = self.image_placedTitle
 
 
 if __name__ == '__main__':
-    pygame.init()
-    pygame.display.set_caption('Движущийся круг 2')
-    size = width, height = 600, 600
     screen = pygame.display.set_mode(size)
     clock = pygame.time.Clock()
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
+        if random.randint(0, 50) == 3:
+            block = Blocks()
         screen.fill((0, 0, 0))
-        all_sprites.draw(screen)
         all_sprites.update()
+        all_sprites.draw(screen)
         clock.tick(60)
         pygame.display.flip()
