2ca4a81466555fae1985415da6ebd2d392145a6c
KLU4V <nikita.ivanus@gmail.com>
2023-12-16 19:38:20 +0300

    Blocks physics update 2

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ size = width, height = 576, 800
 running = True
 
 blocks = pygame.sprite.Group()
+fallen_blocks = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
 hero = pygame.sprite.Group()
 items = pygame.sprite.Group()
@@ -37,26 +38,48 @@ class Blocks(pygame.sprite.Sprite):
     image = pygame.transform.scale(image, (48, 48))
 
     def __init__(self):
+
         super().__init__(all_sprites)
+
+        x = random.randint(1, 10)
+
+        self.rotate_angle = random.randint(1, 4) * 90
+
+        self.image = pygame.transform.rotate(self.image, self.rotate_angle)
+        self.image_placedTitle = pygame.transform.rotate(self.image_placedTitle, self.rotate_angle)
+
         self.add(blocks)
+
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
-        flag = True
-        while flag:
-            self.rect.x = random.randint(1, 10) * 48
+
+        self.movement_flag = False
+        self.transformed = False
+
+        while self.movement_flag is False:
             for i in blocks:
-                if pygame.sprite.collide_rect(self, i):
-                    flag = False
-                else:
-                    flag = True
-        self.rect.y = 0
+                if pygame.sprite.collide_mask(self, i):
+                    self.movement_flag = True
+                    self.rect.x = x * 48
+            x = random.randint(1, 10)
 
     def update(self):
-        if 0 <= self.rect.y + 48 < 800:
-            self.rect = self.rect.move(0, 2)
+        for i in fallen_blocks:
+            if pygame.sprite.collide_rect(self, i):
+                self.add(fallen_blocks)
+                self.remove(blocks)
+                self.movement_flag = False
+                self.image = self.image_placedTitle
 
-        elif self.rect.y + 48 == 800:
-            self.image = self.image_placedTitle
+        if self.movement_flag:
+            if 0 <= self.rect.y + 48 < 800:
+                self.rect = self.rect.move(0, 2)
+
+            elif self.rect.y + 48 == 800:
+                self.image = self.image_placedTitle
+                self.add(fallen_blocks)
+                self.remove(blocks)
+                self.movement_flag = False
 
 
 if __name__ == '__main__':
@@ -66,10 +89,10 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        if random.randint(0, 50) == 3:
+        if random.randint(0, 40) == 3:
             block = Blocks()
         screen.fill((0, 0, 0))
         all_sprites.update()
         all_sprites.draw(screen)
-        clock.tick(60)
+        clock.tick(200)
         pygame.display.flip()
