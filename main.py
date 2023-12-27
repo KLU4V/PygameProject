887a0b2bed5710814144ef5e48b9f36f8bf9d4b8
KLU4V <nikita.ivanus@gmail.com>
2023-12-27 16:22:04 +0300

    Character update 4: >added animations >reassembled character's jump physics >some general improvements

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+import os
+import sys
 from pygame.locals import *
 import random
 
@@ -18,27 +20,87 @@ possibilities = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
 def load_image(name, colorkey=None):
-    image = pygame.image.load(name)
-    image = pygame.transform.scale(image, (36, 64))
+    fullname = os.path.join(name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
     return image
 
 
 class Hero(pygame.sprite.Sprite):
-    image = load_image("hero_copy.png")
+    right = load_image("graphics/animations/default/default_right.png")
+    right = pygame.transform.scale(right, (32, 56))
+    left = load_image("graphics/animations/default/default_left.png")
+    left = pygame.transform.scale(left, (32, 56))
+
+    run_left = [load_image("graphics/animations/left/1.png"),
+                load_image("graphics/animations/left/2.png"),
+                load_image("graphics/animations/left/3.png"),
+                load_image("graphics/animations/left/4.png"),
+                load_image("graphics/animations/left/5.png"),
+                load_image("graphics/animations/left/6.png"),
+                load_image("graphics/animations/left/7.png"),
+                load_image("graphics/animations/left/8.png")]
+
+    for i in range(len(run_left)):
+        run_left[i] = pygame.transform.scale(run_left[i], (32, 56))
+
+    run_right = [load_image("graphics/animations/right/1.png"),
+                 load_image("graphics/animations/right/2.png"),
+                 load_image("graphics/animations/right/3.png"),
+                 load_image("graphics/animations/right/4.png"),
+                 load_image("graphics/animations/right/5.png"),
+                 load_image("graphics/animations/right/6.png"),
+                 load_image("graphics/animations/right/7.png"),
+                 load_image("graphics/animations/right/8.png")]
+
+    for i in range(len(run_right)):
+        run_right[i] = pygame.transform.scale(run_right[i], (32, 56))
+
+    death = [load_image("graphics/animations/death/1.png"),
+             load_image("graphics/animations/death/2.png"),
+             load_image("graphics/animations/death/3.png"),
+             load_image("graphics/animations/death/4.png")]
+
+    for i in range(len(death)):
+        death[i] = pygame.transform.scale(death[i], (32, 56))
+
+    blink = [load_image("graphics/animations/blink/eyes.png"),
+             load_image("graphics/animations/blink/void.png")]
+
+    for i in range(len(blink)):
+        blink[i] = pygame.transform.scale(blink[i], (32, 56))
+
+    jumpAn = [load_image("graphics/animations/jump/1.png"),
+              load_image("graphics/animations/jump/2.png"),
+              load_image("graphics/animations/jump/3.png"),
+              load_image("graphics/animations/jump/4.png"),
+              load_image("graphics/animations/jump/5.png"),
+              load_image("graphics/animations/jump/6.png"),
+              load_image("graphics/animations/jump/7.png"),
+              load_image("graphics/animations/jump/8.png")]
+
+    for i in range(len(jumpAn)):
+        jumpAn[i] = pygame.transform.scale(jumpAn[i], (32, 56))
 
     def __init__(self):
         super().__init__(all_sprites)
 
         self.add(hero)
+        self.image = self.right
 
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
 
         self.direction_x = 0
         self.direction_y = 1
-        self.moved = False
         self.tick_jump_counter = 1
-        self.jump_flag = False
+        self.jump_counter = 0
+
+        self.moved = False
+        self.jump_flag = [False, 'stand', 0]
+        self.falling_flag = [False, "stand"]
 
         self.rect.x = 100
         self.rect.y = 600
@@ -48,23 +110,59 @@ class Hero(pygame.sprite.Sprite):
             self.rect.x += nx
             self.direction_x = nx
             self.moved = True
+            print(nx)
 
     def jump(self):
-        self.rect.y -= 5
-        self.jump_flag = True
+        self.jump_counter += 1
+        if self.jump_counter <= 1:
+            if self.moved is True:
+                if self.direction_x > 0:
+                    self.jump_flag = [True, "right", 10]
+                else:
+                    self.jump_flag = [True, "left", 10]
+            else:
+                self.jump_flag = [True, "stand", 10]
 
     def check_air(self):
         if not pygame.sprite.spritecollideany(self,
-                                              fallen_blocks) and self.rect.y + 64 != 800 and self.jump_flag is False:
-            self.rect.y += 1
+                                              fallen_blocks) and self.rect.y + 64 != 800 and self.jump_flag[0] is False:
+            self.rect.y += 3
+            if self.falling_flag[0] is True:
+                if self.falling_flag[1] == "right" and 0 <= self.rect.x + 3 <= 800:
+                    self.rect.x += 3
+                elif self.falling_flag[1] == "left" and 0 <= self.rect.x - 3 <= 800:
+                    self.rect.x -= 3
 
-        elif self.jump_flag is True and self.tick_jump_counter != 8:
-            self.rect.y -= 5
+            if pygame.sprite.spritecollideany(self,
+                                              fallen_blocks) or self.rect.midbottom[1] == 800:
+                self.rect.y -= 3
+                self.jump_counter -= 1
+                self.falling_flag = [False, "stand"]
+
+        elif self.jump_flag[0] is True and self.jump_flag[1] == "stand" and self.tick_jump_counter != 10:
+            self.rect.y -= self.jump_flag[2]
             self.tick_jump_counter += 1
+            self.jump_flag[2] -= 1
+            self.image = self.jumpAn[5]
 
-        elif self.tick_jump_counter == 8:
+        elif self.jump_flag[0] is True and self.jump_flag[1] == "right" and self.tick_jump_counter != 10:
+            self.rect.y -= self.jump_flag[2]
+            self.tick_jump_counter += 1
+            self.jump_flag[2] -= 1
+            self.rect.x += 3
+            self.image = self.jumpAn[5]
+
+        elif self.jump_flag[0] is True and self.jump_flag[1] == "left" and self.tick_jump_counter != 10:
+            self.rect.y -= self.jump_flag[2]
+            self.tick_jump_counter += 1
+            self.jump_flag[2] -= 1
+            self.rect.x -= 3
+            self.image = self.jumpAn[5]
+
+        elif self.tick_jump_counter == 10:
             self.tick_jump_counter = 1
-            self.jump_flag = False
+            self.falling_flag = [True, self.jump_flag[1]]
+            self.jump_flag = [False, 'stand', 10]
 
     def check_ground(self):
         if self.moved is True:
@@ -95,15 +193,6 @@ class Hero(pygame.sprite.Sprite):
                 return True
             return False
 
-    # def check_blocks(self):
-    #     for i in blocks:
-    #         while pygame.sprite.collide_rect(self, i):
-    #             if (not i.rect.midbottom[1] != self.rect.midtop[1] + 1 and not i.rect.x <= self.rect.x <= i.rect.x
-    #                     and self.rect.midbottom[1] != i.rect.midtop[1] + 1 and pygame.sprite.collide_rect(self, i)):
-    #                 self.rect.x -= self.direction_x
-    #             else:
-    #                 break
-
 
 class Camera:
     def __init__(self):
@@ -119,9 +208,9 @@ class Camera:
 
 
 class Block(pygame.sprite.Sprite):
-    image_placedTitle = load_image("placed_tile.png")
+    image_placedTitle = load_image("graphics/textures/placed_tile.png")
     image_placedTitle = pygame.transform.scale(image_placedTitle, (48, 48))
-    image_fallingTitle = load_image("tile.png")
+    image_fallingTitle = load_image("graphics/textures/tile.png")
     image_fallingTitle = pygame.transform.scale(image_fallingTitle, (48, 48))
 
     def __init__(self):
@@ -231,7 +320,8 @@ def main():
 
     screen = pygame.display.set_mode(size)
     clock = pygame.time.Clock()
-    dead = False
+    dead = [False, 0, 0]
+    anim_counter_rl = [0, 0, "right"]
 
     for value in blocks_dct.values():
         if value[1] is True:
@@ -239,28 +329,56 @@ def main():
             value[0].rect.y = 752
 
     while running:
-        if not dead:
+        if dead[0] is False:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_SPACE:
-                        character.jump()
-                    if event.key == pygame.K_w:
-                        character.jump()
-                    character.check_ground()
+
+                if not (pygame.key.get_pressed()[K_d] or pygame.key.get_pressed()[K_a]):
+                    if anim_counter_rl[2] == "right":
+                        character.image = character.right
+                        anim_counter_rl = [0, 0, "right"]
+                    else:
+                        character.image = character.left
+                        anim_counter_rl = [0, 0, "left"]
+
                 if pygame.key.get_pressed()[K_a]:
                     character.run(-4)
+                    anim_counter_rl[1] += 1
+                    anim_counter_rl[2] = "left"
+
+                    if anim_counter_rl[0] <= 7 and anim_counter_rl[1] % 4 == 0:
+                        character.image = character.run_left[anim_counter_rl[0]]
+                        anim_counter_rl[0] += 1
+                    elif anim_counter_rl[0] >= 8:
+                        anim_counter_rl[0] = 0
+
                 if pygame.key.get_pressed()[K_d]:
                     character.run(4)
+                    anim_counter_rl[1] += 1
+                    anim_counter_rl[2] = "right"
+
+                    if anim_counter_rl[0] <= 7 and anim_counter_rl[1] % 4 == 0:
+                        character.image = character.run_right[anim_counter_rl[0]]
+                        anim_counter_rl[0] += 1
+                    elif anim_counter_rl[0] >= 8:
+                        anim_counter_rl[0] = 0
+
                 if pygame.key.get_pressed()[K_s]:
                     character.lower()
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        character.jump()
+
+                    elif event.key == pygame.K_w:
+                        character.jump()
 
             character.check_air()
             character.check_ground()
 
-            if character.check_death():
-                dead = True
+            if character.check_death() or dead[0] is True:
+                dead[0] = True
 
             if random.randint(0, 40) == 3:
                 for value in blocks_dct.values():
@@ -291,6 +409,18 @@ def main():
             all_sprites.update()
             all_sprites.draw(screen)
 
+        elif dead[0] is True and dead[1] <= 3:
+            if dead[2] % 10 == 0:
+                character.image = character.death[dead[1]]
+                dead[1] += 1
+            dead[2] += 1
+            dead[0] = True
+
+            screen.fill((0, 0, 0))
+            all_sprites.update()
+            all_sprites.draw(screen)
+
+
         else:
             for c, value in enumerate(blocks_dct.values()):
                 value[0].default()
@@ -304,15 +434,17 @@ def main():
                     value[0].spawn()
                     value[0].rect.y = 752
             character.rect.y = 650
+            character.image = character.right
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_r:
-                        dead = False
+                        dead = [False, 0, 0]
 
-            f = pygame.font.Font("Silkscreen-Regular.ttf", 36)
+            f = pygame.font.Font("graphics/fonts/Silkscreen-Regular.ttf"
+                                 , 36)
             deadtext = f.render('R to restart', True,
                                 (255, 255, 255))
             screen.blit(deadtext, (150, 400))
